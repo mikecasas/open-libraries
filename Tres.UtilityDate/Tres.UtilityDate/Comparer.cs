9b8313a3cdd03fd96807662bb95d4addf9a74398
Mike Casas <civicprogrammer@gmail.com>
2019-10-05 17:58:55 -0400

    New project for formatting dates.

---
+++ Tres.UtilityDate/Tres.UtilityDate/Comparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Tres.UtilityDate
+{
+    public static class Comparer
+    {     
+      
+        public static bool IsBetween(this DateTime i, DateTime begin, DateTime end)
+        {
+            return i >= begin && i<= end;
+        }        
+
+        public static bool IsBefore(this DateTime i, DateTime val)
+        {
+            return i >= begin && i <= end;
+        }
+
+        public static bool IsOnOrBefore(this DateTime i, DateTime val)
+        {
+            return i >= begin && i <= end;
+        }
+
+        public static bool IsAfter(this DateTime i, DateTime val)
+        {
+            return i >= begin && i <= end;
+        }
+    }
+}
