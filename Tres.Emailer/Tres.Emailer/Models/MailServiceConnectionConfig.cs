4c3f1f76efc8b8fb9e18b0c473cfe42c0d7cc278
Mike Casas <civicprogrammer@gmail.com>
2019-10-13 17:05:58 -0400

    Update.

---
+++ Tres.Emailer/Tres.Emailer/Models/MailServiceConnectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tres.Emailer.Models
+{
+    class MailServiceConnectionConfig
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool UseSsl { get; set; }
+    }
+}
